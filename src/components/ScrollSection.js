8d684e327197ad970aa2f9ebb8f1e799b81238cb
Bodin Hsiao <sandy900825@gmail.com>
2022-12-15 22:25:41 +0800

    1

--- src/components/ScrollSection.js
+++ src/components/ScrollSection.js
@@ -4,8 +4,16 @@ import "./ScrollSection.css";
 function ScrollSection({ backgroundColor }) {
   return (
     <div className="section" style={{ backgroundColor: backgroundColor }}>
-      <div className="copy">
-        <h1>Lorem ipsum</h1>
+      <div className="themeInfo">
+        <div>
+          <div className="themeTitle">未來道路選擇</div>
+          <div className="themeIntro">
+            繼續升學增進自我?
+            <br /> 開始工作走進社會?
+            <br /> 畢業後我該選擇往哪個道路走呢?
+          </div>
+        </div>
+        <div className="navButton">看看學長姐如何選擇</div>
       </div>
     </div>
   );
