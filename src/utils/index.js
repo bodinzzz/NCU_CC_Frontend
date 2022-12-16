f32ce790480c6de2ab7eee729f0802de85b5c05b
Xiugapurin <jason32567810@gmail.com>
2022-12-16 15:15:20 +0800

    add util/index.js(description of colors/text)

---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+export const Colors = {
+  // General
+  white: "#FFFFFF",
+  background: "#F8F8F8",
+  unselected: "#C5C5C5",
+  // For Text
+  textPrimary: "#686868",
+  // For Themes
+  themeOneMain: "#E37464",
+  themeOneSecondary: "#FFFDEF",
+  themeTwoMain: "#789EE7",
+  themeTwoSecondary: "#D4E2FC",
+  themeThreeMain: "#00BCD4",
+  themeThreeSecondary: "#EBFAFF",
+  // For Charts
+  themeTwoChartFirst: "#789EE7",
+  themeTwoChartSecond: "#AAC7FF",
+  themeTwoChartThird: "#D4E2FC",
+  themeTwoChartOther: "#E4E4E4",
+};
+
+export const TextStyle = {};
