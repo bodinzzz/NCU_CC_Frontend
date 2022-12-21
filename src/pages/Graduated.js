8145408b064a4dc313dd3a163a0abc24041a9cc9
Bodin Hsiao <sandy900825@gmail.com>
2022-12-21 21:34:38 +0800

    graduated-page-1 finished

--- src/pages/Graduated.js
+++ src/pages/Graduated.js
@@ -1,11 +1,16 @@
 import "./Graduated.scss";
+import PageOne from "../components/graduated/PageOne";
 
-function Graduated() {
+function Future() {
   return (
-    <div className="container">
-      <h1>Graduated</h1>
+    <div className="graduated scroll-container">
+      <div className="graduated__section-1">
+        <PageOne />
+      </div>
+      <div className="graduated__section-2"></div>
+      <div className="graduated__section-3"></div>
     </div>
   );
 }
 
-export default Graduated;
+export default Future;
