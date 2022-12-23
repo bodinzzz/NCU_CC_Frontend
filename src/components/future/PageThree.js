31e30aaec4dac2bd4b8c253dca37429bb04f703a
Bodin Hsiao <sandy900825@gmail.com>
2022-12-23 23:27:05 +0800

    adjust chart tooltip

--- src/components/future/PageThree.js
+++ src/components/future/PageThree.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 import LearnerImg from "../../assets/image/Future/LearnerImg.svg";
 import SoldierImg from "../../assets/image/Future/SoldierImg.svg";
 import UnemployedImg from "../../assets/image/Future/UnemployedImg.svg";
@@ -15,10 +8,22 @@ import RetireesImg from "../../assets/image/Future/RetireesImg.svg";
 import TravelerImg from "../../assets/image/Future/TravelerImg.svg";
 import "./PageThree.scss";
 
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <span className="label">{`${label} : ${payload[0].value}`}%</span>
+      </div>
+    );
+  }
+
+  return null;
+};
+
 function Chart({ data }) {
   return (
     <ResponsiveContainer width="60%" height={400}>
-      <BarChart data={data} layout={"vertical"} barSize={32}>
+      <BarChart data={data} layout={"vertical"} barSize={24}>
         <YAxis
           type="category"
           dataKey="name"
@@ -31,7 +36,7 @@ function Chart({ data }) {
           }}
         />
         <XAxis type="number" hide />
-        <Tooltip cursor={false} />
+        <Tooltip content={<CustomTooltip />} cursor={false} />
         <Bar dataKey="percentage" fill="#96B48B" />
       </BarChart>
     </ResponsiveContainer>
@@ -69,41 +74,16 @@ function PageThree() {
   return (
     <div className="future-page-3">
       <div className="future-page-3__title">
-        <span>工學院 軟體工程研究所 學士 </span>在
-        <span>畢滿一年後 其他現況分布</span>:
+        <span>工學院 軟體工程研究所 學士 </span>在<span>畢滿一年後 其他現況分布</span>:
       </div>
       <div className="future-page-3__chart">
         <div className="future-page-3__chart__icons">
-          <img
-            src={LearnerImg}
-            className="future-page-3__chart__icons__icon"
-            alt={LearnerImg}
-          />
-          <img
-            src={SoldierImg}
-            className="future-page-3__chart__icons__icon"
-            alt={SoldierImg}
-          />
-          <img
-            src={UnemployedImg}
-            className="future-page-3__chart__icons__icon"
-            alt={UnemployedImg}
-          />
-          <img
-            src={HousekeeperImg}
-            className="future-page-3__chart__icons__icon"
-            alt={HousekeeperImg}
-          />
-          <img
-            src={RetireesImg}
-            className="future-page-3__chart__icons__icon"
-            alt={RetireesImg}
-          />
-          <img
-            src={TravelerImg}
-            className="future-page-3__chart__icons__icon"
-            alt={TravelerImg}
-          />
+          <img src={LearnerImg} className="future-page-3__chart__icons__icon" alt={LearnerImg} />
+          <img src={SoldierImg} className="future-page-3__chart__icons__icon" alt={SoldierImg} />
+          <img src={UnemployedImg} className="future-page-3__chart__icons__icon" alt={UnemployedImg} />
+          <img src={HousekeeperImg} className="future-page-3__chart__icons__icon" alt={HousekeeperImg} />
+          <img src={RetireesImg} className="future-page-3__chart__icons__icon" alt={RetireesImg} />
+          <img src={TravelerImg} className="future-page-3__chart__icons__icon" alt={TravelerImg} />
         </div>
         <Chart data={data} />
       </div>
