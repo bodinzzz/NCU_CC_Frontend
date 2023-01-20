086500c7944fe18fd10e0fc386d04f81145d9b8c
Bodin Hsiao <sandy900825@gmail.com>
2023-01-20 16:37:38 +0800

    rename data

--- src/components/career/PageFour.js
+++ src/components/career/PageFour.js
@@ -7,35 +7,35 @@ import WorldMap from "../../assets/map/WorldMap.json";
 import SourceTooltip from "../elements/SourceTooltip";
 
 // TO SORT DATA ADD GET FILL COLOR
-function sortData(data) {
-  data.sort((a, b) => (a.percentage < b.percentage ? 1 : -1));
-  // const fillColors = ["#0096C7", "#00b4d8", "#48cae4", "#90e0ef", "#ade8f4", "#caf0f8"];
-  const fillColors = ["#0096C7", "#cdb4db", "#ffc8dd", "#ffafcc", "#bde0fe", "#a2d2ff"];
-  var fillColor = "black";
+// function sortData(data) {
+//   data.sort((a, b) => (a.percentage < b.percentage ? 1 : -1));
+//   // const fillColors = ["#0096C7", "#00b4d8", "#48cae4", "#90e0ef", "#ade8f4", "#caf0f8"];
+//   const fillColors = ["#0096C7", "#cdb4db", "#ffc8dd", "#ffafcc", "#bde0fe", "#a2d2ff"];
+//   var fillColor = "black";
 
-  const sortedData = data.map((data, index) => {
-    if (data.percentage >= 80) {
-      fillColor = fillColors[0];
-    } else if (data.percentage < 80 && data.percentage >= 60) {
-      fillColor = fillColors[1];
-    } else if (data.percentage < 60 && data.percentage >= 40) {
-      fillColor = fillColors[2];
-    } else if (data.percentage < 40 && data.percentage >= 20) {
-      fillColor = fillColors[3];
-    } else if (data.percentage < 20 && data.percentage >= 1) {
-      fillColor = fillColors[4];
-    } else if (data.percentage < 1 && data.percentage >= 0) {
-      fillColor = fillColors[5];
-    }
-    return {
-      name: data.name,
-      percentage: data.percentage,
-      isHover: data.isHover,
-      fill: fillColor,
-    };
-  });
-  return sortedData;
-}
+//   const sortedData = data.map((data, index) => {
+//     if (data.percentage >= 80) {
+//       fillColor = fillColors[0];
+//     } else if (data.percentage < 80 && data.percentage >= 60) {
+//       fillColor = fillColors[1];
+//     } else if (data.percentage < 60 && data.percentage >= 40) {
+//       fillColor = fillColors[2];
+//     } else if (data.percentage < 40 && data.percentage >= 20) {
+//       fillColor = fillColors[3];
+//     } else if (data.percentage < 20 && data.percentage >= 1) {
+//       fillColor = fillColors[4];
+//     } else if (data.percentage < 1 && data.percentage >= 0) {
+//       fillColor = fillColors[5];
+//     }
+//     return {
+//       name: data.name,
+//       percentage: data.percentage,
+//       isHover: data.isHover,
+//       fill: fillColor,
+//     };
+//   });
+//   return sortedData;
+// }
 
 function PageFour() {
   const tooltipText = `資料來源 : 中央大學民國109至111年畢業流向調查結果\n學士有效問卷200份(回收率70%)\n碩士有效問卷200份(回收率70%)\n博士有效問卷200份(回收率70%)\n地區分布與男女比為學碩博綜合統計\n畢業滿1.3.5年合併統計`;
@@ -43,83 +43,92 @@ function PageFour() {
   // MAP JSON FILE
   const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
 
-  // RAW DATA
-  const data = [
-    {
-      name: "China", //亞洲（香港QQ、澳門、大陸地區）
-      percentage: 95,
-    },
-    {
-      name: "Asia", //亞洲（香港、澳門、大陸地區以外國家）
-      percentage: 85,
-    },
-    {
-      name: "Australia", //大洋洲
-      percentage: 75,
-    },
-    {
-      name: "Africa", //非洲
-      percentage: 65,
-    },
-    {
-      name: "Australia", //大洋洲
-      percentage: 55,
-    },
-    {
-      name: "Europe", //歐洲
-      percentage: 45,
-    },
-    {
-      name: "North America", //北美洲
-      percentage: 35,
-    },
-    {
-      name: "Central America", //中美洲
-      percentage: 25,
-    },
-    {
-      name: "South America", //南美洲
-      percentage: 0,
-    },
-  ].map((data, index) => {
-    return {
-      name: data.name,
-      percentage: data.percentage,
-      isHover: false,
-    };
-  });
-
-  // const sortedData = sortData(data);
-  const [sortedData, setSortedData] = useState(sortData(data));
+  const [continentData, setContinentData] = useState(
+    [
+      {
+        name: "China", //亞洲（香港QQ、澳門、大陸地區）
+        percentage: 95,
+        fillColor: "blue",
+      },
+      {
+        name: "Asia", //亞洲（香港、澳門、大陸地區以外國家）
+        percentage: 85,
+        fillColor: "red",
+      },
+      {
+        name: "Australia", //大洋洲
+        percentage: 75,
+        fillColor: "green",
+      },
+      {
+        name: "Africa", //非洲
+        percentage: 65,
+        fillColor: "yellow",
+      },
+      {
+        name: "Australia", //大洋洲
+        percentage: 55,
+        fillColor: "orange",
+      },
+      {
+        name: "Europe", //歐洲
+        percentage: 45,
+        fillColor: "gray",
+      },
+      {
+        name: "North America", //北美洲
+        percentage: 35,
+        fillColor: "purple",
+      },
+      {
+        name: "Central America", //中美洲
+        percentage: 25,
+        fillColor: "red",
+      },
+      {
+        name: "South America", //南美洲
+        percentage: 0,
+        fillColor: "pink",
+      },
+    ].map((data, index) => {
+      return {
+        ...data,
+        isHover: false,
+      };
+    })
+  );
 
   // GET FILL COLOR WITH SINGLE COUNTRY'S CONTINENT
   function getFill(continent) {
     var fillColor = "black";
-    if (continent === "Asia") {
-      fillColor = sortedData[0].fill;
-    } else if (continent === "China") {
-      fillColor = sortedData[1].fill;
+    // console.log(typeof continent);
+    // return sortedData[sortedData.findIndex((item) => item.name === "China")].fill;
+    // return sortedData[sortedData.findIndex((item) => item.name === continent)].fill;
+    if (continent === "China") {
+      fillColor = continentData[0].fillColor;
+    } else if (continent === "Asia") {
+      fillColor = continentData[1].fillColor;
     } else if (continent === "Australia") {
-      fillColor = sortedData[2].fill;
+      fillColor = continentData[2].fillColor;
     } else if (continent === "Africa") {
-      fillColor = sortedData[3].fill;
+      fillColor = continentData[3].fillColor;
     } else if (continent === "Europe") {
-      fillColor = sortedData[4].fill;
+      fillColor = continentData[4].fillColor;
     } else if (continent === "North America") {
-      fillColor = sortedData[5].fill;
+      fillColor = continentData[5].fillColor;
     } else if (continent === "Central America") {
-      fillColor = sortedData[6].fill;
+      fillColor = continentData[6].fillColor;
     } else if (continent === "South America") {
-      fillColor = sortedData[7].fill;
+      fillColor = continentData[7].fillColor;
     }
     return fillColor;
   }
 
   // 移進去一個國家跟移出去一個國家都會改isHover=!isHover(這裡有成功)
-  function updateSortedData(key) {
-    const sortedDataCopy = [...sortedData];
-    sortedDataCopy[key].isHover = !sortedDataCopy[key].isHover;
-    setSortedData(sortedDataCopy);
+  function updateContinentData(key) {
+    const continentDataCopy = [...continentData];
+    continentDataCopy[key].isHover = !continentDataCopy[key].isHover;
+    setContinentData(continentDataCopy);
   }
 
   return (
@@ -145,24 +154,20 @@ function PageFour() {
                     strokeWidth="0.5"
                     // MOUSE EVENTS
                     onMouseEnter={() => {
-                      sortedData.map((data, key) => {
+                      continentData.map((data, key) => {
                         if (WorldMap.features[index].properties.continent === data.name) {
-                          updateSortedData(key);
+                          updateContinentData(key);
                         }
                       });
                     }}
                     onMouseLeave={() => {
-                      sortedData.map((data, key) => {
+                      continentData.map((data, key) => {
                         if (WorldMap.features[index].properties.continent === data.name) {
-                          updateSortedData(key);
+                          updateContinentData(key);
                         }
                       });
                     }}
-                    className={
-                      sortedData[sortedData.findIndex((item) => item.name === WorldMap.features[1].properties.continent)].isHover
-                        ? "geographies-style-hover"
-                        : "geographies-style"
-                    }
+                    className={continentData[1].isHover ? "geographies-style-hover" : "geographies-style"}
                   />
                 ))
               }
