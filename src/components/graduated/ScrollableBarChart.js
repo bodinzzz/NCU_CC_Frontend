a6954580a2d1d65f1100da7e888ad01cdf1df217
Bodin Hsiao <sandy900825@gmail.com>
2023-02-04 20:57:14 +0800

    scrollableBarChart test RWD using calculate

--- src/components/graduated/ScrollableBarChart.js
+++ src/components/graduated/ScrollableBarChart.js
@@ -15,30 +15,13 @@ const CustomizedTick = ({ x, y, payload }) => {
 };
 
 function ScrollableBarChart({ data }) {
-  //Discipline size range -> 1~27
-  //Career size range -> 1~16
   const size = Object.keys(data).length;
-  console.log(size);
-  let customHeight = 0;
-  if (size > 16) {
-    customHeight = 2000;
-  } else {
-    customHeight = 600;
-  }
-  console.log(customHeight);
+  let customHeight = 87 * size;
   return (
     <div className="scrollable-bar-chart__scroll-wrapper">
       <ResponsiveContainer width="100%" height={customHeight}>
         <BarChart data={data} layout={"vertical"} barSize={30} reverseStackOrder="false" margin={{ top: 20, right: 50, left: 20 }}>
-          <YAxis
-            type="category"
-            dataKey="name"
-            axisLine={false}
-            tick={<CustomizedTick />}
-            tickLine={false}
-            // tickMargin={10}
-            width={1}
-          />
+          <YAxis type="category" dataKey="name" axisLine={false} tick={<CustomizedTick />} tickLine={false} width={1} />
           <XAxis type="number" hide />
           <Tooltip content={<ChartTooltip />} cursor={false} outline={"none"} />
           <Bar dataKey="percentage" className="scrollable-bar-chart__bar">
